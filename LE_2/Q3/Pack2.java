a4b9191072140ec30b1ac7a12c2b2fcf5fbc5404
221B348 <221B348@juet.co.in>
2025-01-29 15:26:31 +0530

    LE_2 Uploaded

---
+++ LE_2/Q3/Pack2.java
@@ -0,0 +1,8 @@
+package com.Q3;
+import com.juet.Pack1;
+public class Pack2 {
+    public static void main(String[] args) {
+        Pack1 obj = new Pack1();
+        obj.display(); 
+    }
+}
---
+++ LE_2/Q3/juet/Pack1.java
@@ -0,0 +1,6 @@
+package com.Q3;
+public class Pack1 {
+    public void display() {
+        System.out.println("Display method from Pack1");
+    }
+}
